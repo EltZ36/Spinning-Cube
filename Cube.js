57b2f5e3cf01a0a0802cfa429d4b9053a1bb9599
Elton <32944003+EltZ36@users.noreply.github.com>
2020-07-16 16:12:47 -0700

    Create Cube.js

---
+++ Cube.js
@@ -0,0 +1,34 @@
+var x;
+function setup(){
+  x = 20;
+  createCanvas(1000,700,WEBGL);
+}
+function draw(){
+  thing();
+  thing(x);
+  shapes();
+}
+function thing(x){
+  background(255);
+  stroke(255,255,255);
+  ambientLight(0,255,255);
+  ambientMaterial(0,0,245);
+  //rotateZ(frameCount * 4);
+  torus(80+x,15);
+  //rotateX(frameCount * 1);
+  //rotateY(frameCount * 2);
+  push();
+  translate(-200,0,0);
+  box(150+x,20+x,50);
+  translate(400,0,0);
+  box(150+x,20+x,50);
+  translate(-200,0,200);
+  pop();
+  pop();
+}
+function shapes(){
+  sphere(40);
+  rotateZ(frameCount * 4);
+  rotateX(frameCount * 1);
+  rotateY(frameCount * 2);
+}
